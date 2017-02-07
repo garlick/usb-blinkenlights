66f65beab5e305a4eca3e882d5d3036e15714970
Jim Garlick <garlick.jim@gmail.com>
2017-02-06 18:50:02 -0800

    firmware_v2: extract osc_calibrate to its own file

--- firmware_v2/main.c
+++ firmware_v2/main.c
@@ -1,3 +1,9 @@
+/* Inspiration (and some code) taken from the following open source projects:
+ * - Adafruit trinket keyboard
+ * - BlinkStick firmware
+ * - V-USB Powerswitch demo project
+ */
+
 #include <avr/power.h>
 #include <util/delay.h>
 
@@ -6,32 +12,41 @@
 
 #include "Light_WS2812/light_ws2812.h"
 
-void usbBegin() {
-        cli();
+#define NUMLEDS 7
 
-        // run at full speed, because Trinket defaults to 8MHz for low voltage compatibility reasons
-        clock_prescale_set(clock_div_1);
+static struct cRGB led[NUMLEDS];
 
-        // fake a disconnect to force the computer to re-enumerate
-        PORTB &= ~(_BV(USB_CFG_DMINUS_BIT) | _BV(USB_CFG_DPLUS_BIT));
-        usbDeviceDisconnect();
-        _delay_ms(250);
-        usbDeviceConnect();
+void test_leds (void);
+void clear_leds (void);
 
-        // start the USB driver
-        usbInit();
-        sei();
+void usbBegin (void)
+{
+    cli();
+
+    // run at full speed, because Trinket defaults to 8MHz for
+    // low voltage compatibility reasons
+    clock_prescale_set(clock_div_1);
+
+    // fake a disconnect to force the computer to re-enumerate
+    PORTB &= ~(_BV(USB_CFG_DMINUS_BIT) | _BV(USB_CFG_DPLUS_BIT));
+    usbDeviceDisconnect();
+    _delay_ms(250);
+    usbDeviceConnect();
+
+    // start the USB driver
+    usbInit();
+    sei();
 }
 
-usbMsgLen_t usbFunctionSetup(uint8_t data[8])
+usbMsgLen_t usbFunctionSetup (uint8_t data[8])
 {
     usbRequest_t *rq = (void *)data;
     static uchar    replyBuf[2];
 
     usbMsgPtr = (unsigned short) replyBuf;
     switch (rq->bRequest) {
-        case 0: // write new LED state
-            return USB_NO_MSG; // defer transfer to usbFunctionWrite()
+        case 0: // re-execute self-test
+            break;
     }
     return 0;
 }
@@ -41,68 +56,7 @@ uchar usbFunctionWrite(uchar *data, uchar len)
     return 0;
 }
 
-#if defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny25__)
-/* ------------------------------------------------------------------------- */
-/* ------------------------ Oscillator Calibration ------------------------- */
-/* ------------------------------------------------------------------------- */
-// section copied from EasyLogger
-/* Calibrate the RC oscillator to 8.25 MHz. The core clock of 16.5 MHz is
- * derived from the 66 MHz peripheral clock by dividing. Our timing reference
- * is the Start Of Frame signal (a single SE0 bit) available immediately after
- * a USB RESET. We first do a binary search for the OSCCAL value and then
- * optimize this value with a neighboorhod search.
- * This algorithm may also be used to calibrate the RC oscillator directly to
- * 12 MHz (no PLL involved, can therefore be used on almost ALL AVRs), but this
- * is wide outside the spec for the OSCCAL value and the required precision for
- * the 12 MHz clock! Use the RC oscillator calibrated to 12 MHz for
- * experimental purposes only!
- */
-void calibrateOscillator(void)
-{
-    uchar       step = 128;
-    uchar       trialValue = 0, optimumValue;
-    int         x, optimumDev, targetValue = (unsigned)(1499 * (double)F_CPU / 10.5e6 + 0.5);
-
-    /* do a binary search: */
-    do{
-        OSCCAL = trialValue + step;
-        x = usbMeasureFrameLength();    /* proportional to current real frequency */
-        if(x < targetValue)             /* frequency still too low */
-            trialValue += step;
-        step >>= 1;
-    }while(step > 0);
-    /* We have a precision of +/- 1 for optimum OSCCAL here */
-    /* now do a neighborhood search for optimum value */
-    optimumValue = trialValue;
-    optimumDev = x; /* this is certainly far away from optimum */
-    for(OSCCAL = trialValue - 1; OSCCAL <= trialValue + 1; OSCCAL++){
-        x = usbMeasureFrameLength() - targetValue;
-        if(x < 0)
-            x = -x;
-        if(x < optimumDev){
-            optimumDev = x;
-            optimumValue = OSCCAL;
-        }
-    }
-    OSCCAL = optimumValue;
-}
-/*
-Note: This calibration algorithm may try OSCCAL values of up to 192 even if
-the optimum value is far below 192. It may therefore exceed the allowed clock
-frequency of the CPU in low voltage designs!
-You may replace this search algorithm with any other algorithm you like if
-you have additional constraints such as a maximum CPU clock.
-For version 5.x RC oscillators (those with a split range of 2x128 steps, e.g.
-ATTiny25, ATTiny45, ATTiny85), it may be useful to search for the optimum in
-both regions.
-*/
-#endif
-
-#define NUMLEDS 7
-
-struct cRGB led[NUMLEDS];
-
-void clear_leds ()
+void clear_leds (void)
 {
     int i;
     for (i = 0; i < NUMLEDS; i++) {
@@ -114,7 +68,9 @@ void clear_leds ()
     _delay_ms(100);
 }
 
-void test_leds ()
+/* Dislpay test pattern on LEDs.
+ */
+void test_leds (void)
 {
     const int t = 200;
     const int b = 10;
@@ -141,7 +97,7 @@ void test_leds ()
     clear_leds ();
 }
 
-int main()
+int main (int argc, char **argv)
 { 
     clock_prescale_set(clock_div_1);
     test_leds ();
