3ab69a245e345e94892f2a2147aad38f453634b8
Jim Garlick <garlick.jim@gmail.com>
2017-02-07 19:25:34 +0000

    firmware_v2: add "millis" timer tick

---
+++ firmware_v2/millis.c
@@ -0,0 +1,36 @@
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/atomic.h>
+#include <inttypes.h>
+
+#include "millis.h"
+
+static volatile uint32_t count;
+
+ISR (TIMER0_COMPA_vect)
+{
+    count++;
+}
+
+void millis_init (void)
+{
+    TCCR0A = (1 << WGM01);             // CTC mode
+    TCCR0B = (1 << CS02);              // prescaler 256
+    OCR0A = F_CPU/256 * 0.001 - 1;     // 1ms compare value
+    TIMSK |= (1<<OCIE0A);              // enable interrupt on compare match
+}
+
+uint32_t millis (void)
+{
+    uint32_t ret;
+
+    ATOMIC_BLOCK (ATOMIC_FORCEON)
+    {
+        ret = count;
+    }
+    return ret;
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
---
+++ firmware_v2/millis.h
@@ -0,0 +1,8 @@
+#include <inttypes.h>
+
+void millis_init (void);
+uint32_t millis (void);
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
